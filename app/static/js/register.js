f928ae8be1801f0914957c493b8e730a197c36ce
Alberto Giannini <alberto.giannini@twtinfo.com.br>
2026-03-11 16:09:20 -0300

    Confirm password implemented

--- app/static/js/register.js
+++ app/static/js/register.js
@@ -1,5 +1,27 @@
-// Placeholder for register logic
-document.getElementById('register-form').addEventListener('submit', function(event) {
+API_URL = "http://127.0.0.1:8000/auth"
+
+const form = document.querySelector('form');
+document.getElementById('register-form').addEventListener('submit', async function(event) {
     event.preventDefault();
-    alert('Register functionality not implemented yet.');
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    if (data.password != data.confirm_password) {
+        alert("As senhas não coincidem.");
+        form.reset();
+        return;
+    } 
+
+    const response = await fetch(`${API_URL}/signup`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+    if (response.ok) {
+        window.location.href = "/login";
+    } else {
+        alert("Erro ao fazer login. Por favor, tente novamente.");
+    }
 });
